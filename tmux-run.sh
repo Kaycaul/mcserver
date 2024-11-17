710c0ac07f5745b7f14b23bd9ab77c618d2daa61
Doeball <doughcatball@gmail.com>
2024-11-17 15:39:34 -0500

    work with any tmux session

--- tmux-run.sh
+++ tmux-run.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
 if [ -z "$TMUX" ]; then
-    echo "Run this script inside the tmux session \"main\""
+    echo "Error: Run this script inside a tmux session."
     exit 1
 fi
+
+SESSION=$(tmux display-message -p '#S')
 
 # run the mcserver docker compose detatched
 docker compose up -d
 
 # set up session and clear window
-tmux kill-window -t main:mcserver
-tmux new-window -t main -n mcserver
+tmux kill-window -t $SESSION:mcserver
+tmux new-window -t $SESSION -n mcserver
 
 # attach to server, playit, exec rcon-cli, and open bash in the new window
-tmux split-window -v -t main:mcserver
-tmux split-window -h -t main:mcserver.0
-tmux split-window -h -t main:mcserver.2
-tmux send-keys -t main:mcserver.0 "docker attach paper" C-m
-tmux send-keys -t main:mcserver.1 "docker attach playit" C-m
-tmux send-keys -t main:mcserver.2 "sleep 30 ; docker exec -it paper rcon-cli" C-m
+tmux split-window -v -t $SESSION:mcserver
+tmux split-window -h -t $SESSION:mcserver.0
+tmux split-window -h -t $SESSION:mcserver.2
+tmux send-keys -t $SESSION:mcserver.0 "docker attach paper" C-m
+tmux send-keys -t $SESSION:mcserver.1 "docker attach playit" C-m
+tmux send-keys -t $SESSION:mcserver.2 "sleep 30 ; docker exec -it paper rcon-cli" C-m
